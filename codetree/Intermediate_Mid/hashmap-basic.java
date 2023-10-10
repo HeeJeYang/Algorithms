7d2c653f14a9e691efe0f13e1cf85cb722ea61a5
HeeJeYang <ghjadmb21@gmail.com>
2023-10-11 08:12:30 +0900

    solved codetree/HashMap

---
+++ codetree/Intermediate_Mid/hashmap-basic.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<Integer, Integer> storage = new HashMap<>();
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String command = sc.next();
+            int a = sc.nextInt();
+            switch (command) {
+                case "add":
+                    int b = sc.nextInt();
+                    storage.put(a, b);
+                    break;
+                case "remove":
+                    storage.remove(a);
+                    break;
+                case "find":
+                    if (storage.containsKey(a)) {
+                        System.out.println(storage.get(a));
+                    } else {
+                    System.out.println("None");
+                    }
+                    break;
+            }
+        }
+    }
+}

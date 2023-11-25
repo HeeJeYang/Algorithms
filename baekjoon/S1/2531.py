3da28030af23914824b296cb5f32f007c1f76999
HeeJeYang <ghjadmb21@gmail.com>
2023-11-26 03:04:17 +0900

    solved baekjoon/회전 초밥 with python

--- baekjoon/S1/2531.py
+++ baekjoon/S1/2531.py
@@ -1,6 +1,6 @@
 # [BOJ] 2531. 회전 초밥
-# 소요 시간 : 00 ms
-# 메모리 : 00 KB
+# 소요 시간 : 108 ms
+# 메모리 : 34036 KB
 
 import sys
 from collections import defaultdict
@@ -14,16 +14,20 @@ max_sushi_type = 0
 for i in range(k):
     sushi_info[sushis[i]] += 1
 max_sushi_type = len(sushi_info.keys())
-if sushi_info[c] == 0:
+
+if not sushi_info.get(c):
     max_sushi_type += 1
 
-for i in range(k, N):
+for i in range(k, N + k):
+    i = i % N
     sushi_info[sushis[i - k]] -= 1
+    if sushi_info[sushis[i - k]] == 0:
+        del sushi_info[sushis[i - k]]
     sushi_info[sushis[i]] += 1
     sushi_type = len(sushi_info.keys())
-    if sushi_info[c] == 0:
+    if not sushi_info.get(c):
         sushi_type += 1
     max_sushi_type = max(max_sushi_type, sushi_type)
 
-print(max_sushi_type - 1)        
+print(max_sushi_type)        
 

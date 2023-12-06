90b97dcc3d989326ed20022a2207ee68fb55658a
HeeJeYang <ghjadmb21@gmail.com>
2023-12-06 19:47:39 +0900

    solved baekjoon/한 줄로 서기

---
+++ baekjoon/S2/1138.py
@@ -0,0 +1,22 @@
+# [BOJ] 1138. 한 줄로 서기
+# 실행 시간 : 00 ms
+# 메모리 : 00 KB
+
+from itertools import permutations
+
+N = int(input())
+people = list(map(int, input().split()))
+
+for p_case in permutations(range(1, N + 1), N):
+    order_list = [0] * N
+    for i in range(N):
+        for j in range(i):
+            if p_case[j] > p_case[i]:
+                order_list[p_case[i] - 1] += 1
+    
+    for i in range(N):
+        if people[i] != order_list[i]:
+            break
+    else:
+        print(*p_case)
+        break
